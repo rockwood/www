1ee0bee909101689fe51f913b02fb52340f8419b
Kevin Rockwood <kevin@rockwood.me>
2016-07-10 12:35:10 +0900

    Add LOCAL_PATH variable for codeship

--- Rakefile.rb
+++ Rakefile.rb
@@ -4,6 +4,7 @@ desc "Publish via rsync"
 task :deploy do
   user = "rockwood"
   server = "50.56.110.128"
-  path = "~/domains/rockwood.me/public"
-  sh("rsync -e 'ssh -p 2222' --compress --recursive --checksum --delete _site/ #{user}@#{server}:#{path}")
+  local_path = ENV.fetch("LOCAL_PATH", "_site")
+  remote_path = "~/domains/rockwood.me/public"
+  sh("rsync -e 'ssh -p 2222' --compress --recursive --checksum --delete #{local_path} #{user}@#{server}:#{remote_path}")
 end
