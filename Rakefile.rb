bd11b8bfc22a76ffd8c9e55d70006116697042e1
Kevin Rockwood <kevin@rockwood.me>
2016-07-10 12:58:45 +0900

    Simplify deploy task by removing configurable path

--- Rakefile.rb
+++ Rakefile.rb
@@ -4,7 +4,6 @@ desc "Publish via rsync"
 task :deploy do
   user = "rockwood"
   server = "50.56.110.128"
-  local_path = ENV.fetch("LOCAL_PATH", "_site")
   remote_path = "~/domains/rockwood.me/public"
-  sh("rsync -e 'ssh -p 2222' --compress --recursive --checksum --delete #{local_path} #{user}@#{server}:#{remote_path}")
+  sh("rsync -e 'ssh -p 2222' --compress --recursive --checksum --delete _site/ #{user}@#{server}:#{remote_path}")
 end
